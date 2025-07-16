a0e3106fd82f023813ad024de4163087be6d46b5
Lam Tran <53012411+SilvaHerald@users.noreply.github.com>
2025-07-16 18:25:32 +0700

    chore: change output migration folder of drizzle-kit (#13)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 // For more info, please read official docs: https://orm.drizzle.team/docs/kit-overview
 export default defineConfig({
   schema: './src/db/schemas',
-  out: './drizzle',
+  out: './db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: 'postgres://username:password@host:port/database',
